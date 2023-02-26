daf64e5841083e3b78340ecddede63a925316158
giangnguyen102995 <94968761+giangnguyen102995@users.noreply.github.com>
2023-02-26 20:58:12 +1100

    use bcrypt to hash all passwords

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
-const express = require("express");	
+const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
-const encrypt = require("mongoose-encryption")
+const bcrypt = require("bcrypt");
+const saltRound = 10;
+
 
 const app = express();
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));  
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 // Users' Schema:
 const userSchema = new mongoose.Schema({
@@ -20,59 +22,60 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-//using the convenient way to use 1 key to encrypt instead of 2
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
-
-
 const User = new mongoose.model("User", userSchema);
 
-
-
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password,
-    });
+app.post("/register", function (req, res) {
+    bcrypt.hash(req.body.password, saltRound, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash,
+        });
 
-    newUser.save(function(err){
-        if (err){
-            console.log(err);
-        } else {
-            res.render("secrets")
-        }
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets")
+            }
+        })
     })
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function (req, res) {
     const userName = req.body.username;
     const password = req.body.password;
 
-    User.findOne({email: userName}, function(err, foundUser){
-        if (err){
+    User.findOne({ email: userName }, function (err, foundUser) {
+        if (err) {
             console.log(err);
-        } else if(foundUser){
-            // we have to use .password to encrypt the search to match the encrypted one
-            if (foundUser.password === password){
-                res.render("secrets");
-            }
+        } else if (foundUser) {
+            // compared hashed passwords with the typed password
+            bcrypt.compare(password, foundUser.password, function(err, results){
+                if (results === true){
+                    res.render("secrets");
+                } else {
+                    res.send("Login credentials not found!")
+                }
+            })
         }
     });
 });
 
 
-app.listen(3000, function(){
+
+app.listen(3000, function () {
 
     console.log("server is running on port 3000...")
 
